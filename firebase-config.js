1edce8c4f260408bec1d9d206fc5b64afa0b3bf8
tallermantenimiento <taller.mantenimiento@sklum.com>
2026-05-18 10:45:03 +0200

    primera version control vehiculos

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Configuración Firebase del proyecto control vehiculos
+// Este archivo debe estar junto a index.html y terminal_vehiculos.html
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnWM0Q5cqvo30NmVFaQ0JmHqLlTN7zG5M",
+  authDomain: "control-vehiculos-70141.firebaseapp.com",
+  projectId: "control-vehiculos-70141",
+  storageBucket: "control-vehiculos-70141.firebasestorage.app",
+  messagingSenderId: "344320891622",
+  appId: "1:344320891622:web:dbe45ff815f0bc5a737927"
+};
